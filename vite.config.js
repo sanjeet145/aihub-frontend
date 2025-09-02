45a491fd46cbfd1d08e70f5107278b90592a4fed
sanjeet145 <sanjeetverma9572@gmail.com>
2025-09-02 23:28:31 +0530

    v0.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react()],
+  server: {
+    allowedHosts: ['aihub-oa8q.onrender.com'],
+    host: '0.0.0.0',  
+    port: 3000       
+  }
+});
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+// })
